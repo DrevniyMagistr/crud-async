f505d5bd043b5e706006b6972307b2269d5a3122
DrevniyMagistr <sasha.klishch@gmail.com>
2022-02-16 18:15:09 +0200

    added start files

---
+++ src/js/api/settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  movieKey: 'e495d779054d343a4536cf2dfe74914b',
+  movieApiUrl: 'https://api.themoviedb.org/3',
+  MOCK_API_URL: 'https://620aa64692946600171c5be5.mockapi.io/api/v1',
+};
+
+export default settings;
